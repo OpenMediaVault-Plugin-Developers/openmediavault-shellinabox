6b6e4903e27d0e00fd98b4aa1beea7e8372b5d2f
mitchtay <mitchtay@localhost.localdomain>
2013-10-17 14:24:17 +1300

    Initial commit.

---
+++ var/www/openmediavault/js/omv/module/admin/system/shell/Settings.js
@@ -0,0 +1,132 @@
+/**
+ * This file is part of OpenMediaVault.
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ *
+ * OpenMediaVault is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OpenMediaVault is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/form/field/CertificateComboBox.js")
+
+/**
+ * @class OMV.module.admin.system.shell.Settings
+ * @derived OMV.workspace.form.Panel
+ */
+
+Ext.define("OMV.module.admin.system.shell.Settings", {
+	extend: "OMV.workspace.form.Panel",
+
+	rpcService: "Shell",
+	rpcGetMethod: "getSettings",
+	rpcSetMethod: "setSettings",
+
+    plugins: [{
+        ptype: "linkedfields",
+        correlations: [{
+            name: [
+                "fontfamily",
+                "fontsize"
+            ],
+            conditions: [
+                { name: 'enabletermsettings', value: true }
+            ],
+            properties: [
+                "!allowBlank",
+                "!readOnly"
+            ]
+         }],
+    }],
+
+	getFormItems: function() {
+		var me = this;
+		return [{
+			xtype: "fieldset",
+			title: _("General settings"),
+			fieldDefaults: {
+				labelSeparator: ""
+			},
+			items: [{
+				xtype: "checkbox",
+				name: "enable",
+				fieldLabel: _("Enable"),
+				checked: false
+			},{
+				xtype: "combo",
+				name: "linkify",
+				fieldLabel: _("Linkify"),
+				store: Ext.create("OMV.data.Store", {
+					fields: [
+						{ name: "value", type: "string" }
+					],
+					data: [
+						{value: "none"},
+						{value: "normal"},
+						{value: "aggressive"}
+					]
+				}),
+				queryMode: "local",
+				displayField: "value",
+				valueField: "value",
+				allowBank: false,
+				typeAhead: true,
+				forceSelection: true,
+				triggerAction: "all",
+				value: "none"
+			},{
+				xtype: "checkbox",
+				name: "enablebeep",
+				fieldLabel: _("Beep"),
+				boxLabel: _("Enable sound on BEL character"),
+				checked: false
+			}],
+		},{
+            xtype: "fieldset",
+            title: _("Terminal settings"),
+            fieldDefaults: {
+                labelSeparator: ""
+            },
+            items: [{
+                xtype: "checkbox",
+                name: "enabletermsettings",
+                fieldLabel: _("Enable"),
+                checked: false
+            },{
+                xtype: "textfield",
+                name: "fontfamily",
+                fieldLabel: _("Font"),
+                value: 'Courier New'
+            },{
+                xtype: "numberfield",
+                name: "fontsize",
+                fieldLabel: _("Size"),
+                minValue: 1,
+                maxValue: 72,
+                allowDecimals: false,
+                allowBlank: false,
+                value: 14
+            }]
+        }];
+	}
+});
+
+OMV.WorkspaceManager.registerPanel({
+	id: "settings",
+	path: "/system/shell",
+	text: _("Settings"),
+	position: 10,
+	className: "OMV.module.admin.system.shell.Settings"
+});
---
+++ var/www/openmediavault/js/omv/module/admin/system/shell/Terminal.js
@@ -0,0 +1,87 @@
+/**
+ * This file is part of OpenMediaVault.
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ *
+ * OpenMediaVault is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OpenMediaVault is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/panel/Panel.js")
+
+/**
+ * @class OMV.module.admin.system.shell.Shell
+ * @derived OMV.workspace.panel.Panel
+ */
+
+Ext.define("OMV.module.admin.system.shell.Terminal", {
+	extend: "OMV.workspace.panel.Panel",
+	requires: [
+		"OMV.Rpc",
+		"OMV.window.MessageBox"
+	],
+
+	initComponent: function() {
+		var me = this;
+		OMV.MessageBox.wait(null, _("Loading ..."));
+		OMV.Rpc.request({
+			scope: me,
+			callback: function(id, success, response) {
+				if(success) {
+					if(response.running == true) {
+						me.embedTerminal();
+					} else {
+						me.embedWarning();
+					}
+				} else {
+					OMV.MessageBox.error(null, error);
+				}
+			},
+			rpcData: {
+				service: "Shell",
+				method: "getStatus"
+			}
+		});
+		
+		me.callParent(arguments);
+	},
+
+	embedTerminal: function() {
+		var me = this;
+
+		me.html = "<form style='overflow: auto; height: 100%;'>";
+		me.html += "  <iframe src='/shell' name='shell' longsec='Shell in a box' width='100%' height='100%'/>";
+		me.html += "<br/></form>"; 
+	},
+
+	embedWarning: function() {
+		var me = this;
+
+		me.html = "<form style='overflow: auto; height: 100%;'>";
+		me.html += "  <div class='x-box-aboutbox'><p>";
+		me.html += "    The shell-in-a-box service is not currently running, please enable it to access the terminal. <br/";
+		me.html += "  </p></div>";
+		me.html += "<br/></form>"; 
+	}
+
+});
+	
+OMV.WorkspaceManager.registerPanel({
+	id: "terminal",
+	path: "/system/shell",
+	text: _("Terminal"),
+	position: 20,
+	className: "OMV.module.admin.system.shell.Terminal"
+});
